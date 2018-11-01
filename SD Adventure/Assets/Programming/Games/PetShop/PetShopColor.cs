23ffc25c118f34d60054f446a2cc30f9f406dd27
Juanpi <jpgc.1302@gmail.com>
2018-10-31 22:13:17 -0500

    Veterinaria 3

---
+++ SD Adventure/Assets/Programming/Games/PetShop/PetShopColor.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetShopColor : PetShopShape
+{
+
+    public Transform PetsParent;
+    protected override void Initialize()
+    {
+        base.Initialize();
+        if(DataManager.IsHardGame)
+            PetsParent.position += Vector3.forward * -1.8f;
+    }
+
+    protected override void CheckHard()
+    {
+        Groups.Clear();
+        for(int i = 0; i < Pets.Length; i++)
+        {
+            Groups.Add(new PetGroup());
+            for(int j = 0; j < Pets.Length; j++)
+            {
+                if(Vector3.SqrMagnitude(Pets[i].position - Pets[j].position) < GroupDistance)
+                {
+                    Groups[Groups.Count - 1].Group.Add(Pets[j]);
+                }
+            }
+            if(Groups[Groups.Count - 1].Group.Count == 0)
+                Groups.RemoveAt(Groups.Count - 1);
+            else
+                Groups[Groups.Count - 1].Group.Sort(delegate (Transform x, Transform y)
+                {
+                    return x.name.CompareTo(y.name);
+                });
+        }
+
+        for(int i = 0; i < Groups.Count; i++)
+        {
+            for(int j = 0; j < Groups.Count; j++)
+            {
+                if(i != j && Groups[i].Compare(Groups[j]))
+                {
+                    Groups.RemoveAt(j);
+                    j--;
+                }
+            }
+        }
+
+
+        int leftovers = 0;
+        for(int i = 0; i < Groups.Count; i++)
+        {
+            if(Groups[i].Group.Count > GroupsSize)
+            {
+                Debug.Log("Group too big");
+                return;
+            }
+
+            if(Groups[i].Group.Count < GroupsSize)
+                leftovers += Groups[i].Group.Count;
+        }
+
+        if(leftovers > GroupsSize - 1)
+        {
+            Debug.Log("Incomplete groups");
+            return;
+        }
+
+
+        string checking = string.Empty;
+
+        for(int i = 0; i < Groups.Count; i++)
+        {
+            if(Groups[i].Group.Count != GroupsSize)
+                continue;
+
+            for(int j = 0; j < Options.Length; j++)
+            {
+                if(Groups[i].Group[0].name.Contains(Options[j]))
+                    checking = Options[j];
+            }
+
+            for(int j = 0; j < Groups[i].Group.Count; j++)
+            {
+                if(!Groups[i].Group[j].name.Contains(checking))
+                {
+                    Debug.Log("No match on group " + i);
+                    return;
+                }
+            }
+        }
+
+        Win();
+    }
+}
--- SD Adventure/Assets/Programming/Games/PetShop/PetShopShape.cs
+++ SD Adventure/Assets/Programming/Games/PetShop/PetShopShape.cs
@@ -10,9 +10,9 @@ public class PetShopShape : BaseGame
 
     DragAndDrop control;
     public string[] OnCompleteKeys = new string[] { "PetShop-1" };
+    public Transform[] Pets;
 
     [Header("Hard")]
-    public Transform[] Pets;
     public List<PetGroup> Groups = new List<PetGroup>();
     public float GroupDistance = 1.2f;
     public int GroupsSize = 3;
@@ -62,123 +62,133 @@ public class PetShopShape : BaseGame
 
     public void Check()
     {
-        Groups.Clear();
         if(DataManager.IsHardGame)
         {
-            for(int i = 0; i < Pets.Length; i++)
-            {
-                Groups.Add(new PetGroup());
-                for(int j = 0; j < Pets.Length; j++)
-                {
-                    if(Vector3.SqrMagnitude(Pets[i].position - Pets[j].position) < GroupDistance)
-                    {
-                        Groups[Groups.Count - 1].Group.Add(Pets[j]);
-                    }
-                }
-                if(Groups[Groups.Count - 1].Group.Count == 0)
-                    Groups.RemoveAt(Groups.Count - 1);
-                else
-                    Groups[Groups.Count - 1].Group.Sort(delegate (Transform x, Transform y)
-                    {
-                        return x.name.CompareTo(y.name);
-                    });
-            }
-
-            for(int i = 0; i < Groups.Count; i++)
-            {
-                for(int j = 0; j < Groups.Count; j++)
-                {
-                    if(i != j && Groups[i].Compare(Groups[j]))
-                    {
-                        Groups.RemoveAt(j);
-                        j--;
-                    }
-                }
-            }
-
-
-            int leftovers = 0;
-            for(int i = 0; i < Groups.Count; i++)
-            {
-                if(Groups[i].Group.Count > GroupsSize)
-                {
-                    Debug.Log("Group too big");
-                    return;
-                }
-
-                if(Groups[i].Group.Count < GroupsSize)
-                    leftovers += Groups[i].Group.Count;
-            }
-
-            if(leftovers > GroupsSize - 1)
-            {
-                Debug.Log("Incomplete groups");
-                return;
-            }
-
-            Win();
+            CheckHard();
         }
         else
         {
-            int total = 0, each = 0;
-
-            for(int i = 0; i < Container.Length; i++)
-            {
-                each = 0;
-                for(int j = 0; j < Pets.Length; j++)
-                {
-                    if(Container[i].bounds.Contains(Pets[j].position))
-                    {
-                        total++;
-                        each++;
-                    }
-                }
-                if(each == 0)
-                {
-                    Debug.Log("No content on " + Container[i].name);
-                    return;
-                }
-            }
-
-            if(total != Pets.Length)
-            {
-                Debug.Log("Unclasified pets");
-                return;
-            }
-
-            string checking = string.Empty;
-
-            for(int i = 0; i < Container.Length; i++)
-            {
-                content.Clear();
-                for(int j = 0; j < Pets.Length; j++)
-                {
-                    if(Container[i].bounds.Contains(Pets[j].position))
-                        content.Add(Pets[j].gameObject);
-                }
-
-                for(int j = 0; j < Options.Length; j++)
-                {
-                    if(content[0].name.Contains(Options[j]))
-                        checking = Options[j];
-                }
-
-                for(int j = 0; j < content.Count; j++)
-                {
-                    if(!content[j].name.Contains(checking))
-                    {
-                        Debug.Log("No match on " + Container[i].name);
-                        return;
-                    }
-                }
-            }
-
-            Win();
+            CheckEasy();
         }
 
     }
 
-    void Win()
+    protected virtual void CheckHard()
+    {
+        Groups.Clear();
+        for(int i = 0; i < Pets.Length; i++)
+        {
+            Groups.Add(new PetGroup());
+            for(int j = 0; j < Pets.Length; j++)
+            {
+                if(Vector3.SqrMagnitude(Pets[i].position - Pets[j].position) < GroupDistance)
+                {
+                    Groups[Groups.Count - 1].Group.Add(Pets[j]);
+                }
+            }
+            if(Groups[Groups.Count - 1].Group.Count == 0)
+                Groups.RemoveAt(Groups.Count - 1);
+            else
+                Groups[Groups.Count - 1].Group.Sort(delegate (Transform x, Transform y)
+                {
+                    return x.name.CompareTo(y.name);
+                });
+        }
+
+        for(int i = 0; i < Groups.Count; i++)
+        {
+            for(int j = 0; j < Groups.Count; j++)
+            {
+                if(i != j && Groups[i].Compare(Groups[j]))
+                {
+                    Groups.RemoveAt(j);
+                    j--;
+                }
+            }
+        }
+
+
+        int leftovers = 0;
+        for(int i = 0; i < Groups.Count; i++)
+        {
+            if(Groups[i].Group.Count > GroupsSize)
+            {
+                Debug.Log("Group too big");
+                return;
+            }
+
+            if(Groups[i].Group.Count < GroupsSize)
+                leftovers += Groups[i].Group.Count;
+        }
+
+        if(leftovers > GroupsSize - 1)
+        {
+            Debug.Log("Incomplete groups");
+            return;
+        }
+
+        Win();
+    }
+
+    protected void CheckEasy()
+    {
+        int total = 0, each = 0;
+
+        for(int i = 0; i < Container.Length; i++)
+        {
+            each = 0;
+            for(int j = 0; j < Pets.Length; j++)
+            {
+                if(Container[i].bounds.Contains(Pets[j].position))
+                {
+                    total++;
+                    each++;
+                }
+            }
+            if(each == 0)
+            {
+                Debug.Log("No content on " + Container[i].name);
+                return;
+            }
+        }
+
+        if(total != Pets.Length)
+        {
+            Debug.Log("Unclasified pets");
+            return;
+        }
+
+        string checking = string.Empty;
+
+        for(int i = 0; i < Container.Length; i++)
+        {
+            content.Clear();
+            for(int j = 0; j < Pets.Length; j++)
+            {
+                if(Container[i].bounds.Contains(Pets[j].position))
+                    content.Add(Pets[j].gameObject);
+            }
+
+            for(int j = 0; j < Options.Length; j++)
+            {
+                if(content[0].name.Contains(Options[j]))
+                    checking = Options[j];
+            }
+
+            for(int j = 0; j < content.Count; j++)
+            {
+                if(!content[j].name.Contains(checking))
+                {
+                    Debug.Log("No match on " + Container[i].name);
+                    return;
+                }
+            }
+        }
+
+        Win();
+    }
+
+    protected void Win()
     {
         for(int i = 0; i < OnCompleteKeys.Length; i++)
             DataManager.AddProgressKey(OnCompleteKeys[i], 1);
