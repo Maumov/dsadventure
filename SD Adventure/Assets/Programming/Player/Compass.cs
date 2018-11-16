012b6c90426cd39d662efbde05384b519bf63cf4
Juanpi <jpgc.1302@gmail.com>
2018-11-16 18:52:11 -0500

    npc, brujula y estrellas

---
+++ SD Adventure/Assets/Programming/Player/Compass.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public Vector3 PlayerOffset;
+    Transform player;
+    Vector3 target;
+
+    IEnumerator Start()
+    {
+        yield return null;
+        KeyEvents[] games = FindObjectsOfType<KeyEvents>();
+        Transform[] t = new Transform[games.Length];
+
+        for(int i = 0; i < games.Length; i++)
+        {
+            t[games[i].transform.GetSiblingIndex() - 1] = games[i].transform;
+        }
+
+        for(int i = 0; i < t.Length; i++)
+        {
+            if(t[i].gameObject.activeSelf)
+                target = t[i].position;
+        }
+
+        if(target == null)
+            gameObject.SetActive(false);
+        else
+            player = FindObjectOfType<PlayerController>().transform;
+    }
+
+    void LateUpdate()
+    {
+        if(target == Vector3.zero)
+            return;
+
+        transform.position = player.position + PlayerOffset;
+        target.y = transform.position.y;
+        transform.LookAt(target);
+    }
+
+}
---
+++ SD Adventure/Assets/Programming/UI/SceneStars.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneStars : MonoBehaviour
+{
+    public string[] Keys;
+    public Sprite[] Stars;
+
+    private void Start()
+    {
+        Image star = GetComponent<Image>();
+        for(int i = 0; i < Keys.Length; i++)
+        {
+            if(DataManager.CheckProgressKey(Keys[i]))
+                star.sprite = Stars[i];
+        }
+    }
+
+}
